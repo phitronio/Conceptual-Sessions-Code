ae0e5e4d55ab0724ee23ece1876656cce3a87f15
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-09-05 13:45:50 +0600

    Added codes of Algo W-1 C-2

---
+++ Batch 5/Algorithm/Week 1-2/visit_kora_jabe_kina.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char graph[1001][1001];
+bool vis[1001][1001];
+
+int n, m;
+
+int dx[4] = {-1, 0, 1, 0}; // row er move
+int dy[4] = {0, 1, 0, -1}; // col er move
+
+bool valid(int x, int y)
+{
+
+    if (x >= 0 && x < n && y >= 0 && y < m && graph[x][y] == '.')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void dfs(int si, int sj)
+{
+    vis[si][sj] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        int ci = si + dx[i]; // children er row
+        int cj = sj + dy[i]; // children er column
+
+        if (valid(ci, cj) && vis[ci][cj] == false)
+        {
+            vis[ci][cj] = true;
+            dfs(ci, cj);
+        }
+    }
+}
+
+void bfs(int si, int sj)
+{
+
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+
+    while (!q.empty())
+    {
+
+        pair<int, int> node = q.front();
+        q.pop();
+        int nrow = node.first;
+        int ncol = node.second;
+
+        for (int i = 0; i < 4; i++)
+        {
+
+            int ci = nrow + dx[i]; // children er row
+            int cj = ncol + dy[i]; // children er column
+
+            if (valid(ci, cj) && vis[ci][cj] == false)
+            {
+                vis[ci][cj] = true;
+                q.push({ci, cj});
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    int source_row, source_column;
+    cin >> source_row >> source_column;
+    int destination_row, destination_column;
+
+    cin >> destination_row >> destination_column;
+
+    dfs(source_row, source_column); // source node
+
+    if (vis[destination_row][destination_column])
+    {
+        cout << "jawa jabe" << endl;
+    }
+    else
+        cout << "jawa jabe na";
+}
