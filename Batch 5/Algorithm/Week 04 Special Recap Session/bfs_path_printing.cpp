6deb60ffdbf48a2a8b1ed17113a7c551e88cb1c1
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-09-28 23:58:32 +0600

    Added necessary comment for Path printing

--- Batch 5/Algorithm/Week 04 Special Recap Session/bfs_path_printing.cpp
+++ Batch 5/Algorithm/Week 04 Special Recap Session/bfs_path_printing.cpp
@@ -65,24 +65,41 @@ int main()
     }
     bfs(0, 0);
 
-    int sti = par[{2, 2}].first, stj = par[{2, 2}].second;
+    if (vis[2][2])
+    { // যদি আমার দেয়া ডেস্টিনেশন টি ভিজিট হয়। এক্ষেত্রে মেট্রিক্সের একদম শেষ প্রান্তকে ডেস্টিনেশন ধরা হয়েছে যেখানে B আছে
 
-    while (graph[sti][stj] != 'A')
-    {
+        // এখন আমরা B যে জায়গায় আছে তা থেকে শুরু করে এর Parent এর মাধ্যমে A তে পোঁছাবো
 
-        graph[sti][stj] = 'X';
+        int xi = 2; // initially B এর row এর পজিশন
+        int xj = 2; // initally B এর column এর পজিশন
 
-        // sti = par[{sti, stj}].first;  // sti is changing
-        // stj = par[{sti, stj}].second; // we are using sti here again.
-        // // which were causing the problem
+        while (true)
+        { // A খুজে না পাওয়া পর্যন্ত একটি ইনফিনিটি লুপ চালাচ্ছি
 
-        int Nsti = par[{sti, stj}].first;
-        stj = par[{sti, stj}].second;
+            int newXi = par[{xi, xj}].first;  // এখন যে পজিশনে আছে অর্থাৎ (2,2) তার Parent এর row এর পজিশন বের করা হলো
+            int newXj = par[{xi, xj}].second; // এখন যে পজিশনে আছে অর্থাৎ (2,2) তার Parent এর row এর পজিশন বের করা হলো
 
-        // this time it wont cause any problem
+            // উপরের লাইন গুলোতে নতুন ভ্যারিয়েবল নেয়ার কারণ হলো আমরা যদি এখানে এইভাবে xi =  par[{xi,xj}].first এইভাবে xi এর মান আপডেট
+            // করা হলে xi এর মান আপডেট হয়ে যাবে
+            // সেক্ষেত্রে পরের লাইনে xj = par[{xi,xj}].second এক্সেস করার সময় par[{xi,xj}] এর xi এর ভ্যালুটি পরিবর্তিত ভ্যালু পাবো।
 
-        sti = Nsti;
-        // now we will update the value of sti
+            // যেমন ধরুন , xi = 2 এবং xj = 2. এক্ষেত্রে  xi =  par[{2,2}].first. xi এর মান চেঞ্জ হয়ে 1 হয়ে গেলো
+            // এর পর xj = par[{1,2}].second এক্সেস হবে পরের লাইনে , যা হওয়ার কথা ছিলো xj = par[{2,2}].second
+
+            // এরপর xi , xj এর মান আপডেট করে তাদের parent cell এ মুভ করলাম
+
+            xi = newXi;
+            xj = newXj;
+
+            if (graph[xi][xj] == 'A')
+            { // Parent এর সেল এ A থাকলে তাকে পরিবর্তনের কোনো প্রয়োজন নেই। এক্ষেত্রে আমরা  ব্রেক করে দিতে পারি।
+                break;
+            }
+
+            // প্যারেন্ট না হলে সেক্ষেত্রে তা Path এর ই একটি অংশ তাই তা চেঞ্জ করে দিলাম
+
+            graph[xi][xj] = 'X';
+        }
     }
 
     for (int i = 0; i < n; i++)
