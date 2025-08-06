07fe50e25098c15eb0c177c198b869c9528eb681
Abul Bashar Atiq <91819259+ab-atiq@users.noreply.github.com>
2025-08-07 00:19:44 +0600

    Update map_set_cmp.cpp

--- Batch 7/DS/Week 6-2/map_set_cmp.cpp
+++ Batch 7/DS/Week 6-2/map_set_cmp.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main()
 {
-    priority_queue<string, vector<string>, cmp_class> pq; // min heap
-    set<string, vector<string>, cmp_class> pq;
-    map<string, vector<string>, cmp_class> pq;
+    priority_queue<string, vector<string>, cmp_class> pq; 
+    set<string, cmp_class> pq;
+    map<string, int, cmp_class> pq;
     return 0;
 }
