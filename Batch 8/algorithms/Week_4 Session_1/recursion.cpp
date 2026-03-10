406f6d50b8262e3cd38e58c8f09892fe19e65ce6
Shubho Saha <128303446+Shubho-Saha@users.noreply.github.com>
2026-03-11 00:04:08 +0600

    Create recursion.cpp

---
+++ Batch 8/algorithms/Week_4 Session_1/recursion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+
+void recur(int n)
+{
+    if (n == 0) return;
+
+    cout<<n<<" ";
+    recur(n-1);
+}
+
+int main()
+{
+    fastIO();
+    int n;
+    cin>>n;
+
+    recur(n);
+    
+    return 0;
+}
