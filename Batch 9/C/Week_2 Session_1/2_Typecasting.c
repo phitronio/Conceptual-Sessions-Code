7854825c33d4ede9189e222ceaffa7df3e2b59ae
Shubho Saha <128303446+Shubho-Saha@users.noreply.github.com>
2026-04-14 00:31:16 +0600

    Add files via upload

---
+++ Batch 9/C/Week_2 Session_1/2_Typecasting.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    // char ch = 'A';
+
+    // int x = ch;
+
+    // int a = 5;
+    // double b = a;
+
+    // printf("%d %lf", x, b);
+
+    // long long int m = 1234567;
+    // int n = m;
+
+    // printf("%d", n);
+
+    int a = 100000;
+    int b = 100000;
+
+    long long int mul =(long long int) a * b;
+    printf("%lld\n", mul);
+
+
+    int x = 17;
+    int y = 5;
+
+    double z = (1.0*x)/y;
+
+    printf("%lf\n", z);
+
+    long long int p = 1000000LL * 1000000;
+    printf("%lld", p);
+
+    return 0;
+}
+
+
