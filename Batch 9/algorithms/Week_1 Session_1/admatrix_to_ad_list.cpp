9516b495d16a2281a619619c5e3cf3edc5180e74
Shubho Saha <128303446+Shubho-Saha@users.noreply.github.com>
2026-08-19 15:21:18 +0600

    Add files via upload

---
+++ Batch 9/algorithms/Week_1 Session_1/admatrix_to_ad_list.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+
+
+int main()
+{
+    fastIO();
+    int n;
+    cin>>n;
+
+    vector<vector<int>> mat(n, vector<int>(n));
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin>>mat[i][j];
+        }
+    }
+
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    vector<vector<int>> adlist(n);
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if (mat[i][j] == 1) {
+                adlist[i].push_back(j);
+            }
+        }
+    }
+
+
+    cout<<"-------------------"<<endl;
+
+    for(int i=0; i<n; i++) {
+        cout<<i<<" : ";
+        for(auto val : adlist[i]) {
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
